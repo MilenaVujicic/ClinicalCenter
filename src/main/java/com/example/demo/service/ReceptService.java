65643c9b283c1ca07aaff2b68ef20f405b073dba
MAndrea98 <mendrei.andrea@gmail.com>
2019-12-01 16:54:35 +0100

    Urađena celokupna overa recepata.

--- src/main/java/com/example/demo/service/ReceptService.java
+++ src/main/java/com/example/demo/service/ReceptService.java
@@ -1,8 +1,11 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.model.Recept;
 import com.example.demo.repository.ReceptRespository;
 
 @Service
@@ -10,4 +13,12 @@ public class ReceptService {
 
 	@Autowired
 	private ReceptRespository receptRespository;
+	
+	public List<Recept> findAll() {
+		return receptRespository.findAll();
+	}
+	
+	public Recept save(Recept recept) {
+		return receptRespository.save(recept);
+	}
 }
