fcd76f34824bc920eac7490b4da1642c67e15a84
MAndrea98 <mendrei.andrea@gmail.com>
2019-12-05 12:54:26 +0100

    Izmenjen u skripti datum za termin

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -27,7 +27,7 @@ insert into sala(ime, opis, klinika_id) values('Sala Op', 'Sala za opercaije', 1
 insert into sala(ime, opis, klinika_id) values('Sala 2', 'Sala za preglede', 1);
 insert into sala(ime, opis, klinika_id) values('Sala 1', 'Sala 1 u novoj klinici', 2);
 
-insert into termin(datum, slobodan, sala_id) values ('1-22-19 9:00:00', true, 1);
+insert into termin(datum, slobodan, sala_id) values ('1-12-19 9:00:00', true, 1);
 insert into termin(datum, slobodan, sala_id) values ('1-12-19 10:00:00', false, 1);
 insert into termin(datum, slobodan, sala_id) values ('1-12-19 11:30:00', true, 1);
 insert into termin(datum, slobodan, sala_id) values ('1-12-19 15:00:00', false, 2);
