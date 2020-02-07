69ef66e681a93e780daa682c9c9966f45f35a6ec
Postin <filip.vozarevic@gmail.com>
2020-02-07 17:34:15 +0100

    Ispravljena greska u importu za transakciju

--- src/main/java/com/example/demo/service/KorisnikService.java
+++ src/main/java/com/example/demo/service/KorisnikService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import java.util.List;
 import java.util.Optional;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
